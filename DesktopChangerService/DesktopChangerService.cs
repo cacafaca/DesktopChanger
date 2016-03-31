a64841221c072722ccd6ea7baae1a0c25a35fdcc
cacafaca <nemanja.simovic@gmail.com>
2016-04-01 00:59:52 +0200

    Initial check in.

---
+++ DesktopChangerService/DesktopChangerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopChangerService
+{
+    public partial class DesktopChangerService : ServiceBase
+    {
+        public DesktopChangerService()
+        {
+            InitializeComponent();
+            messageListener = new MessageListener();
+        }
+
+        MessageListener messageListener;
+
+        protected override void OnStart(string[] args)
+        {
+            messageListener.Start();
+        }
+
+        protected override void OnStop()
+        {
+            messageListener.Stop();
+        }
+
+    }
+}
---
+++ DesktopChangerService/MessageListener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DesktopChangerService
+{
+    public class MessageListener
+    {
+        public MessageListener()
+        {
+            CanWork = false;
+            IPAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            MessageListenSocket = new TcpListener(IPAddress, 2525);
+        }
+
+        public void Start()
+        {
+            CanWork = true;
+            Listener = new Thread(new ThreadStart(DoListen));
+            Listener.Start();
+        }
+
+        public void Stop()
+        {
+            CanWork = false;
+        }
+
+        Thread Listener;
+        bool CanWork;
+
+        private void DoListen()
+        {
+            int buffSize = 512;
+            while (CanWork)
+            {
+                Thread.Sleep(10);
+                TcpClient tcpClient = MessageListenSocket.AcceptTcpClient();
+                byte[] receiveData = new byte[buffSize];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(receiveData, 0, buffSize);
+
+            }
+        }
+
+        TcpListener MessageListenSocket;
+        IPAddress IPAddress;
+    }
+}
